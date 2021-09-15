07a8ec5a853122bc691c608089496f935bb2607f
Cristian Cuello <cristiancuello10@gmail.com>
2021-09-15 19:33:42 -0300

    logo

--- src/components/topbar/Topbar.jsx
+++ src/components/topbar/Topbar.jsx
@@ -6,7 +6,7 @@ const Topbar = () => {
     <div className="topbar">
       <div className="topbarWrapper">
         <div className="topLeft">
-          <span className="logo">lamaadmin</span>
+          <span className="logo">Admin Dash</span>
         </div>
         <div className="topRight">
           <div className="topbarIconContainer">
